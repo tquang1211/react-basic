f1d158bb1c57dae0c87681346e57c60b01b8b860
Quang Tran <tquang1211@gmail.com>
2023-08-02 14:32:37 +0700

    State

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,10 +1,40 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+    /*
+    JSX => return block (chỉ trả về 1 khối, vd: ko đc trả về 2 div)
+    Nếu mà muốn trả về 2 div, ... thì dùng <React.Fragment></React.Fragment> hoặc <></>
+    */
+    //object => key : value
+    //this sẽ gọi tới class MyComponent để lấy state name hoặc channel
+    state = {
+        name: 'Messi',
+        channel: 'Demo React Channel',
+
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        });
+    }
+
     render() {
         //return <h1>Hello, {this.props.name}</h1>;
         return (
-            <div>Hello my component Demo</div>
+            <>
+                <div className='changeName'>
+                    <input value={this.state.name} type='text'
+                        onChange={(event) => this.handleOnChangeName(event)}
+                    />
+                </div>
+                <div className='first'>
+                    Hello my component, my name is {this.state.name}.
+                </div>
+                <div className='second'>
+                    My youtube channel: {this.state['channel']}.
+                </div>
+            </>
         )
     }
 }
