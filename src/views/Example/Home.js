060e8bf50c9e6e7400571fa28fa0c510e484281f
Quang Tran <tquang1211@gmail.com>
2023-08-11 13:42:01 +0700

    Chuyển Trang - Redirect Với React.JS Routers

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { withRouter } from "react-router";
 
 class Home extends React.Component {
+    componentDidMount() {
+        setTimeout(() => {
+            this.props.history.push('/todo')
+        }, 3000)
+    }
+
+    //HOC: higher order component
     render() {
+        console.log('>>> check props: ', this.props)
         return (
             <div>
                 Hello world from home page with Demo
@@ -10,4 +19,4 @@ class Home extends React.Component {
     }
 }
 
-export default Home;
+export default withRouter(Home);
